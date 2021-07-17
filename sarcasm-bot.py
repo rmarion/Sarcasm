c7154d62fdffe87c8e86dd6b7e52fcdbf5ac88fe
rmarion-dev <development@rmarion.com>
2021-07-16 21:15:20 -0400

    Adding mention functionality

--- sarcasm-bot.py
+++ sarcasm-bot.py
@@ -1,13 +1,31 @@
 from discord.ext import commands
+from discord import User
 from importlib import import_module
 import_module('sarcastic')
 from sarcastic import change_phrase_casing
 
+
 token = open('token', 'r').readline()
 client = commands.Bot(command_prefix = '!')
 
+
 @client.command()
-async def sarcasm(ctx, arg):
+async def sarcasm(ctx, arg: str):
     await ctx.send(change_phrase_casing(arg))
+
+
+@client.command()
+async def sarcasm_target(ctx, user: User):
+    oldest_message = await get_oldest_message(ctx, user.id)
+    await ctx.send(change_phrase_casing(oldest_message))
+
+
+async def get_oldest_message(ctx, id):
+    messages = await ctx.channel.history(limit=200)
+    user_messages = [message for message in messages if message.author.id == id]
+    if not user_messages:
+        return "No recent message found for user."
+    return user_messages[-1]
+
 
 client.run(token)
